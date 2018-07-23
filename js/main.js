13ed07c3b79a0fe5d170ce5fa616bace7b91e2b4
menghuan <1572482184@qq.com>
2018-07-23 23:05:47 +0800

    两真的时间差

--- js/main.js
+++ js/main.js
@@ -4,19 +4,31 @@ let can2;
 let ctx1;
 let ctx2;
 
+let lastTime;
+let deltaTime;
+
 document.body.onload = game;
 function game() {
-	init()
+	init();
+	lastTime = Date.now();
+	deltaTime = 0;
+	gameloop();
 }
 
 function init() {	
 	// 获得canvas context
-	can1 = querySelector('#canvas1'); // first, dust, UI, circle
-	cx1 = can1.getContext();
-	can1 = querySelector('#canvas2'); // first, dust, UI, circle
-	cx1 = can1.getContext();
+	can1 = document.getElementById('canvas1'); // first, dust, UI, circle
+	ctx1 = can1.getContext("2d");
+	can2 = document.getElementById('canvas2'); // background, are, fruits
+	ctx2 = can2.getContext("2d");
+
 }
 
 function gameloop() {
 	requestAnimFrame(gameloop); //setInterval, setTimeout
+	let now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;
+
+	console.log(deltaTime);
 }
